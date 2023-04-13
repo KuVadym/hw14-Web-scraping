50a56f0d09e2ec55035e63005055e5d929dfad25
Vadym Kuzyk <kuzik94vadim@gmail.com>
2023-04-13 16:56:37 +0300

    continue work with new scraping script

--- scrapper_from_wikimedia.py
+++ scrapper_from_wikimedia.py
@@ -1,22 +1,37 @@
-from bs4 import BeautifulSoup
-import requests
+# from bs4 import BeautifulSoup
+# import requests
 
-url = 'https://commons.wikimedia.org/wiki/Rhododendron'
-response = requests.get(url)
+# url = 'https://commons.wikimedia.org/wiki/Rhododendron'
+# response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'lxml')
+# soup = BeautifulSoup(response.text, 'lxml')
 
-imgs = soup.find_all('a', class_="image")
+# imgs = soup.find_all('a', class_="image")
 
-links = []
-for img in imgs:
-    # print(dir(img))
-    row_list = str(img).split(" ")
-    link = ''
-    for el in row_list:
-        if el.startswith("src="):
-            link = el
-            links.append(link)
+# links = []
+# for img in imgs:
+#     # print(dir(img))
+#     row_list = str(img).split(" ")
+#     link = ''
+#     for el in row_list:
+#         if el.startswith("src="):
+#             link = el
+#             links.append(link)
 
-for l in links:
-    print(l)
+# for l in links:
+#     print(l)
+
+my_div_str = "div, div, div"
+
+
+div_list = []
+def div_parser(div):
+    
+    if div.find_all("div"):
+        div = el.find_all("div")
+        div_parser(div)
+        return div
+    else: 
+        return div_list.append(div)
+    
+div_parser(my_div_str)
--- web_scraping_spyder_task_fromupwork/main.py
+++ web_scraping_spyder_task_fromupwork/main.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-url = "https://academy.cs.cmu.edu/docs/canvas"
+# url = "https://academy.cs.cmu.edu/docs/canvas"
+url = "https://academy.cs.cmu.edu/docs/generalShapeProperties"
 driver = webdriver.Chrome(r"c:\path\to\chromedriver.exe")
 driver.get(url)
 
@@ -14,12 +15,65 @@ soup = BeautifulSoup(driver.page_source, "html.parser")
 page = soup.find("div", class_="docs-article content")
 page = page.find("div")
 result = ''
-print(len(page))
-for el in page:
 
+
+div_list = []
+def div_parser(div):
+    global div_list
+
+    for el in div:
+        if el.find_all("div", class_="inline_editor", recursive=False):
+            div_list.append(el)    
+
+        elif el.find_all("div"):
+            div = el.find_all("div", recursive=False)
+            div_parser(div)
+
+        else: 
+            div_list.append(el)
+
+
+div_parser(page)
+
+
+# for el in div_list:
+#     code = ''
+    # text = ''
+    # if el.find_all("div", class_="ace_line"):
+    #     for div in el:
+    #         if not div.find_all("div", class_="ace_gutter-cell"):
+    #             text = div.text
+    #         code = div.find_all("div", class_="ace_layer ace_text-layer")
+    # else:
+    # if not el.find_all("div", class_="ace_gutter-cell"):
+    #     text = el.text
+
+    # if code:
+    #     for el in code:
+    #         result += el.text
+    #         result += "\n"
+    #     result += "\n"
+
+    # if text:
+    #     result += text
+    #     result += "\n"
+
+
+for el in page:    
     code = el.find_all("div", class_="ace_line")
+    img_scription = el.find_all("div", class_="docs-image-notes")
+    head = el.find_all("h3")
     text = el.find_all("p")
-    
+
+    if img_scription:
+        for el in img_scription:
+            result += el.text
+            result += "\n"
+
+    if head:
+        for el in head:
+            result += el.text
+            result += "\n"
     if code:
         for el in code:
             result += el.text
